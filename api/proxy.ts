7a63cf50ebdfe7309f824db6582ac40cfcba48f6
rainbowdev <rainbowskyworks@gmail.com>
2023-09-12 17:23:25 +1000

    Cosmetic changes to serverless function /api/proxy

--- api/proxy.ts
+++ api/proxy.ts
@@ -2,13 +2,13 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import axios from 'axios'
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  // const { name = 'World' } = req.query
-  // res.status(200).send(`Hello ${name}!`)
   try {
     const { backendUrl } = req.query
 
-    if(!backendUrl) {
-      return res.status(400).json({ error: 'Missing backendUrl query parameter'})
+    if (!backendUrl) {
+      return res
+        .status(400)
+        .json({ error: 'Missing backendUrl query parameter' })
     }
 
     //Forward the request to the backend server using Axios
@@ -16,19 +16,13 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
       method: req.method,
       url: backendUrl as string,
       headers: req.headers,
-      data: req.body
+      data: req.body,
     })
 
     //Pass the response from the backend server back to the client
     res.status(response.status).json(response.data)
   } catch (error) {
     console.error('Error: ', error)
-    res.status(500).json({ error: 'Internal Server Error'})
+    res.status(500).json({ error: 'Internal Server Error' })
   }
-  // res.status(200).json({
-  //   body: req.body,
-  //   url: req.url,
-  //   query: req.query,
-  //   cookies: req.cookies,
-  // })
 }
